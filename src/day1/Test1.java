745f80677af15cbbf41eb533dfb4991cedc49c07
ZulfiyyaM <z.moylamova@yahoo.com>
2020-08-18 17:09:59 -0500

    print your name

--- src/day1/Test1.java
+++ src/day1/Test1.java
@@ -1,4 +1,9 @@
 package day1;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Zulfiyya");
+    }
 }
